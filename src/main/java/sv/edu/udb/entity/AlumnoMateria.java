51a15e953ae0e7eec0a2dfd65e7c77aac037d557
RicardoSlazar <alvrh23@gmail.com>
2026-05-22 23:36:47 -0600

    Agregar entidades del sistema

---
+++ src/main/java/sv/edu/udb/entity/AlumnoMateria.java
@@ -0,0 +1,33 @@
+package sv.edu.udb.entity;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlumnoMateria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name="alumno_id")
+    private Alumno alumno;
+
+
+    @ManyToOne
+    @JoinColumn(name="materia_id")
+    private Materia materia;
+
+
+    @Column(nullable=false)
+    private Double nota;
+
+}
